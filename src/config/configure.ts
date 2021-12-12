cb298dc98a4f3d7e0040b20ace95f859a20de4f7
BriceBrice <brice@robby.ai>
2021-12-12 18:16:09 +0100

    [TEST]

--- src/config/configure.ts
+++ src/config/configure.ts
@@ -1,5 +1,5 @@
 const DEFAULT_SERVER_PORT = 3005;
 
 export default () => ({
-  port: parseInt(process.env.PORT, 10) || DEFAULT_SERVER_PORT,
+  port: parseInt(process.env.PORT, 10) || DEFAULT_SERVER_PORT
 });
